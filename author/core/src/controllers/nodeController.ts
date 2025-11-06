58bb5b69b1e2647b120bc62dbcfe81907b0a851e
Sebastian Nowak <sebnow91@gmail.com>
2025-11-06 23:33:04 +0100

    21: Update getNode

--- author/core/src/controllers/nodeController.ts
+++ author/core/src/controllers/nodeController.ts
@@ -5,31 +5,50 @@ import * as fs from 'node:fs';
 // get Node under path
 export const getNode = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const path = req.path;
+    const requestPath = req.path;
     const contentRoot = config.contentRoot;
-    const fullPath = contentRoot + path;
+    const fullPath = contentRoot + requestPath;
     const contentJsonFullPath = fullPath + '/.content.json';
 
     // sprawdz czy podana sciezka istnieje
     // sprawdz czy to katalog czy plik
-    // jesli katalog to zwroc dzieci katalogu (?) i ich conten.json wtedy?
+    // jesli katalog to zwroc scr node typu directory i content json dzieci pierwszego poziomu
     // jesli plik to zwroc zawartosc pliku
     if (fs.existsSync(fullPath)) {
       const stats = fs.statSync(fullPath);
-      // const contentStats = fs.existsSync(contentJsonFullPath);
       if (stats.isDirectory()) {
-        res.json({
-          message: 'Directory exists',
-          fullPath,
-          contentJsonFullPath,
-        });
+        const entries = fs.readdirSync(fullPath, { withFileTypes: true });
+
+        const children = entries
+          .filter((entry) => !entry.name.startsWith('.'))
+          .map((entry) => ({
+            name: entry.name,
+            path: requestPath + entry.name,
+            type: entry.isDirectory() ? 'aemm:folder' : 'aemm:file',
+          }));
+
+        let contentData;
+        try {
+          const data = fs.readFileSync(contentJsonFullPath, 'utf8');
+          contentData = JSON.parse(data);
+        } catch (err) {
+          // .content.json doesn't exist, use default
+          contentData = {
+            'scr:type': 'aemm:folder',
+            path: requestPath,
+          };
+        }
+
+        contentData.children = children;
+
+        res.json(contentData);
         return;
       } else if (stats.isFile()) {
-        res.json({ message: 'File exists', fullPath, contentJsonFullPath });
+        const fileContent = fs.readFileSync(fullPath, 'utf-8');
+        res.json(JSON.parse(fileContent));
         return;
       }
     }
-
     res.json({ fullPath, contentJsonFullPath });
   } catch (error) {
     next(error);
